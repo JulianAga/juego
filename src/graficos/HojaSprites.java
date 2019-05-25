8a7ee7c6694ff35fc08d41439f2d66e3e1fc8b2a
JulianAga <48849848+JulianAga@users.noreply.github.com>
2019-05-25 11:34:21 -0300

    Initial commit

---
+++ src/graficos/HojaSprites.java
@@ -0,0 +1,42 @@
+package graficos;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class HojaSprites {
+	private final int ancho;
+	private final int alto;
+	public final int[] pixeles;
+	
+	//coleccion de hojas de sprites
+	public static HojaSprites ejemplo = new HojaSprites("/texturas/SpritePrueba.png",500,500);
+	
+	//fin de la coleccion
+	
+	public HojaSprites(final String ruta, final int ancho, final int alto)
+	{
+		this.ancho = ancho;
+		this.alto = alto;
+		
+		pixeles = new int [ancho * alto];
+		
+		BufferedImage imagen;
+		try {
+			imagen = ImageIO.read(HojaSprites.class.getResource(ruta));
+			
+			imagen.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public int obtenerAncho()
+	{
+		return ancho;
+	}
+
+}
